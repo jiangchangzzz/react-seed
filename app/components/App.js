3a2ce1e317fcf950b14be0a430cf89aa68abb0bc
jc <1015312525@qq.com>
2017-05-25 20:55:35 +0800

    start

---
+++ app/components/App.js
@@ -0,0 +1,12 @@
+import React,{Component} from 'react';
+import styles from './App.scss';
+
+class App extends Component{
+    render(){
+        return (
+            <div className={styles.blue}>blue in world</div>
+        );
+    }
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,93 @@
+const path = require('path');
+const webpack=require('webpack');
+const HtmlWebpackPlugin=require('html-webpack-plugin');
+const OpenBrowserPlugin=require('open-browser-webpack-plugin');
+
+module.exports = {
+  entry: './app/main.js',
+  output: {
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: [
+          'babel-loader'
+        ]
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true
+            }
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: function(){
+                return [require('autoprefixer')];
+              }
+            }
+          }
+        ]
+      },
+      {
+        test: /\.scss/,
+        exclude: /node_modules/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true
+            }
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: function(){
+                return [require('autoprefixer')];
+              }
+            }
+          },
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+
+  devtool: 'eval-source-map',
+
+  plugins: [
+    //html模板插件
+    new HtmlWebpackPlugin({
+      template: __dirname+'/app/index.html'
+    }),
+
+    //热加载插件
+    new webpack.HotModuleReplacementPlugin(),
+
+    //打开浏览器插件
+    new OpenBrowserPlugin({
+      url: 'http://localhost:8080'
+    })
+  ],
+
+  devServer: {
+    historyApiFallback: true, 
+    inline: true, 
+    hot: true
+  }
+        
+};
